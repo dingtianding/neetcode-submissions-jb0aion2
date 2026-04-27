c1b46c70f1fb659e4c317a39b23e19ac2e0439e4
Ding Tian Ding <dingtian.ding@gmail.com>
2026-04-27 19:24:25 -0400

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,15 @@
+from functools import cache
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            rob = nums[i] + dfs(i + 2)
+            skip = dfs(i + 1)
+            return max(rob, skip)
+
+        return dfs(0)
+            
+        
