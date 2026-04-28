fbbb12d80cce01a9aec24572ac50c867273f14ce
Ding Tian Ding <dingtian.ding@gmail.com>
2026-04-28 01:03:00 -0400

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        cur = 0
+        res = nums[0]
+
+        for num in nums:
+            if cur < 0 :
+                cur = 0
+            cur += num
+            res = max(res, cur)
+
+
+        return res
+        
