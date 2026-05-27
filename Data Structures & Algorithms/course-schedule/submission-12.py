71a8a78fd471bcddbe3f1c5afaaeb612e5084b86
Ding Tian Ding <dingtian.ding@gmail.com>
2026-05-27 16:13:55 -0400

    Add: course-schedule - submission-12

---
+++ Data Structures & Algorithms/course-schedule/submission-12.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = [0] * numCourses
+
+        adj = [[] for i in range(numCourses)]
+
+        for crs, pre in prerequisites:
+            indegree[pre] += 1
+            adj[crs].append(pre)
+
+        q = deque()
+        for n in range(numCourses):
+            if indegree[n] == 0:
+                q.append(n)
+
+        finish = 0
+
+        while q:
+            crs = q.popleft()
+            finish += 1
+            for nei in adj[crs]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+
+        return finish == numCourses
